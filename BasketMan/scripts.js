61f3b8b6bd2ed47b38ac209971591785fdefcce2
Alex1995markson <alexmarksonit@gmail.com>
2020-04-21 23:49:30 +0300

    Add mockup of website

--- BasketMan/scripts.js
+++ BasketMan/scripts.js
@@ -3,5 +3,4 @@ window.addEventListener("load", function(event) {
     gsap.to(".box-text  .tx-big", {rotation: 0, x: 550, duration: 3, scale:1.2, ease: "none"});
     gsap.to(".box-text  .tx-you", {rotation: 0, x: 550, duration: 3, scale:1.2, ease: "none"});
     gsap.to(".box-text .left-padding", {rotation: 0, x: 550, duration: 5, scale:1.2, ease: "none"});
-
-  });
+});
